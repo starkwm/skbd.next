3ddb90a0e50b062b9429b52257772cebd2295d9e
Tom Bell <tomb@tombell.dev>
2025-08-13 00:08:06 +0100

    Add token type enum and token struct
    The token struct is used by the lexer for lexical parsing of the
    configuration file.

---
+++ Sources/SkbdCore/Token.swift
@@ -0,0 +1,20 @@
+enum TokenType {
+  case modifier
+  case key
+  case keyHex
+  case literal
+  case command
+  case plus
+  case dash
+  case directive
+  case beginList
+  case endList
+  case string
+  case unknown
+  case endOfStream
+}
+
+struct Token {
+  var type: TokenType
+  var text: String?
+}
